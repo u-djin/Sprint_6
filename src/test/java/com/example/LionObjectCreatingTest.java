086af94e031ecdc974991225a3eb8e4ce842e10e
Евгений Андреев <eandreev@adalisk.local>
2023-06-11 04:35:49 +0300

    rename LionOneParameterNegativeTest; update test with object checking

--- src/test/java/com/example/LionObjectCreatingTest.java
+++ src/test/java/com/example/LionObjectCreatingTest.java
@@ -12,12 +12,12 @@ import org.mockito.MockitoAnnotations;
 import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(Parameterized.class)
-public class LionOneParameterNegativeTest {
+public class LionObjectCreatingTest {
     String sex;
     @Mock
     private Feline feline;
 
-    public LionOneParameterNegativeTest(String sex) {
+    public LionObjectCreatingTest(String sex) {
         this.sex = sex;
     }
 
@@ -39,6 +39,7 @@ public class LionOneParameterNegativeTest {
     public void LionSexTest() throws Exception {
         try {
             Lion lion = new Lion(feline, sex);
+            Assert.assertNotNull(lion);
         } catch (Exception e) {
             MatcherAssert.assertThat(sex, allOf(not(equalTo("Самец")), not(equalTo("Самка"))));
             Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
